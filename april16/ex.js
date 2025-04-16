77c48f15cfc81096081fd753237d2e8aff474415
Kishanprakash <109814608+Kishanprakash@users.noreply.github.com>
2025-04-16 21:37:17 +0530

    web techno

---
+++ april16/ex.js
@@ -0,0 +1,22 @@
+var age =10;
+let gender ='male'
+
+const institute ='x-workz'
+document .written(age)
+var nama=rakesh
+let emp1 = {id:12,employeeName:'aa',salary:10000}
+
+let emp2 = new Object();
+emp2.id =13;
+emp2.employeeName ='bb';
+emp2.salary = 50000
+emp2.id = 14;
+
+function emp3(id,employeeName,salary){
+    let emp2 = new Object();
+    this.id =id;
+    this.employeeName = employeeName;
+    this.salary = salary;
+}
+
+
